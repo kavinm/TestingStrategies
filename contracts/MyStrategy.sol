2086b08873eefcfbca2f3f99ba34e96f96c4ad83
kavinm <kavin1810@gmail.com>
2021-06-21 22:41:53 -0400

    deposit and withdraw functions implemented

--- contracts/MyStrategy.sol
+++ contracts/MyStrategy.sol
@@ -21,8 +21,8 @@ contract MyStrategy is BaseStrategy {
     using SafeMathUpgradeable for uint256;
 
     // address public want // Inherited from BaseStrategy, the token the strategy wants, swaps into and tries to grow
-    address public lpComponent; // Token we provide liquidity with
-    address public reward; // Token we farm and swap to want / lpComponent
+    address public aToken; // Token we provide liquidity with
+    address public reward; // Token we farm and swap to want / aToken
     address public constant LENDING_POOL =
         0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9; //lending pool address
 
@@ -45,7 +45,7 @@ contract MyStrategy is BaseStrategy {
 
         /// @dev Add config here
         want = _wantConfig[0];
-        lpComponent = _wantConfig[1];
+        aToken = _wantConfig[1];
         reward = _wantConfig[2];
 
         performanceFeeGovernance = _feeConfig[0];
@@ -71,7 +71,8 @@ contract MyStrategy is BaseStrategy {
 
     /// @dev Balance of want currently held in strategy positions
     function balanceOfPool() public view override returns (uint256) {
-        return 0;
+        return IERC20Upgradeable(aToken).balanceOf(address(this));
+        //returns balance of Aave token of this contract's address
     }
 
     /// @dev Returns true if this strategy requires tending
@@ -88,7 +89,7 @@ contract MyStrategy is BaseStrategy {
     {
         address[] memory protectedTokens = new address[](3);
         protectedTokens[0] = want;
-        protectedTokens[1] = lpComponent;
+        protectedTokens[1] = aToken;
         protectedTokens[2] = reward;
         return protectedTokens;
     }
@@ -116,12 +117,22 @@ contract MyStrategy is BaseStrategy {
     /// @dev invest the amount of want
     /// @notice When this function is called, the controller has already sent want to this
     /// @notice Just get the current balance and then invest accordingly
-    function _deposit(uint256 _amount) internal override {}
+    function _deposit(uint256 _amount) internal override {
+        ILendingPool(LENDING_POOL).deposit(want, _amount, address(this), 0); // (asset, amount, onBehalfOf, referralCode)
+
+        // we are casting LendingPool address into ILendingPool interface
+    }
 
     /// @dev utility function to withdraw everything for migration
-    function _withdrawAll() internal override {}
+    function _withdrawAll() internal override {
+        ILendingPool(LENDING_POOL).withdraw(
+            want,
+            balanceOfPool(),
+            address(this)
+        ); //(asset, amount, to)
+    }
 
-    /// @dev withdraw the specified amount of want, liquidate from lpComponent to want, paying off any necessary debt for the conversion
+    /// @dev withdraw the specified amount of want, liquidate from aToken to want, paying off any necessary debt for the conversion
     function _withdrawSome(uint256 _amount)
         internal
         override
